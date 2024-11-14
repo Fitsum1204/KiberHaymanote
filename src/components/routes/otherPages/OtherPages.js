dba54f0285eb052e5973ba031eed93f7579fabfd
Fitsum1204 <fitsumzione24@gmail.com>
2024-11-14 13:37:55 +0300

    fixing error

--- src/components/routes/otherPages/OtherPages.js
+++ src/components/routes/otherPages/OtherPages.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState,Link } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 const OtherPages = () => {
     const [showDropdown, setShowDropdown] = useState({
     
